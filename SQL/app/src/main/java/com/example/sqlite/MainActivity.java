7699f457eaceb3f227255e936437963c11dbcf24
unknown <thedohuynh@gmail.com>
2022-10-24 10:20:08 +0700

    contacts

---
+++ SQL/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+
+import com.example.sqlite.sqlite.DBHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        database.close();
+    }
+}
